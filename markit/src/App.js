53ac437270c24955af288fb0959c78d40037bd31
JohananAP <84732788+JohananAP@users.noreply.github.com>
2023-10-05 17:02:36 -0400

    pushing code

---
+++ markit/src/App.js
@@ -0,0 +1,75 @@
+import "./App.css";
+import React from "react";
+import Login from "./components/user-profile/Login";
+import Register from "./components/user-profile/Register";
+import Profile from "./components/user-profile/Profile";
+import ResetPassword from "./components/user-profile/ResetPassword";
+import Home from "./components/pages/Home";
+import { Switch, Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import JobApplicationStepper from "./components/jobApplication/JobApplicationStepper";
+import ApplicationTabs from "./components/myApplication/tab"
+import NavigationBar from './navbar';
+import Course from './components/courses/Course';
+import CourseDetail from './components/courses/CourseDetail';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+  backgroundImage: 'url({home})',
+}));
+
+function App() {
+  const classes = useStyles();
+  return (
+    <div className="App">
+      <header>
+        <NavigationBar></NavigationBar>
+      </header>
+
+      {/* <AppBar className="nav-bar" position="static" color="blue">
+        
+      </AppBar> */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="/register">
+          <Register></Register>
+        </Route>
+        <Route path="/profile">
+          <Profile></Profile>
+        </Route>
+        <Route path="/reset">
+          <ResetPassword></ResetPassword>
+        </Route>
+        <Route exact path="/myApplication" exact component={ApplicationTabs} />
+        <Route exact path="/myApplication" exact >
+          <ApplicationTabs />
+        </Route>
+
+        <Route path="/jobApplication/:courseName/:jobPosition" exact component={JobApplicationStepper} />
+        <Route
+          exact path={"/courses"}
+          component={() => <Course />}
+        />
+        <Route
+          exact path={"/courses/:id"}
+          component={() => <CourseDetail />}
+        />
+        <Route>
+          <h1>Invalid URL</h1>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ markit/src/navbar.js
@@ -0,0 +1,91 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import DoneIcon from '@material-ui/icons/Done';
+import React from "react";
+import { useState, useEffect } from "react";
+import { useHistory } from 'react-router';
+import './App.css';
+
+
+const NavigationBar = () => {
+    const [toggle, setToggle] = useState(false);
+    const [display, setdisplay] = useState(false);
+    const [number, setNumber] = useState(0);
+    const history = useHistory();
+    console.log('Navbar:' + sessionStorage.getItem('markit-email'));
+    console.log(number);
+
+    const increment = () => {
+        if (sessionStorage.getItem('markit-email') == null || sessionStorage.getItem('markit-email') == "") {
+            setNumber(number + 1);
+            setdisplay(false);
+        }
+        else {
+            setdisplay(true);
+        }
+    }
+
+    const handleToggle = () => {
+        console.log('hanndletoggle');
+        if (sessionStorage.getItem('markit-email')) {
+            setdisplay(false);
+            sessionStorage.setItem('markit-email', null);
+            history.goForward('/login');
+        }
+    }
+
+    useEffect(() => {
+        increment();
+    }, [number])
+
+    if (display) {
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+                <Container>
+                    <Navbar.Brand href="#home" style={{ 'color': '#ffffff' }}>
+                        MarkIT
+                        <DoneIcon fontSize="large"></DoneIcon>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#home">HOME</Nav.Link>
+                            <Nav.Link href="/#/courses">COURSES</Nav.Link>
+                            <Nav.Link href="/#/myApplication">MY APPLICATION</Nav.Link>
+                            <Nav.Link href="#hiring">HIRING</Nav.Link>
+                            <Nav.Link href="#onBoarding">ONBOARDING</Nav.Link>
+                            <Nav.Link href="/#/profile">PROFILE</Nav.Link>
+                            <Nav.Link href="/#/reset">RESET</Nav.Link>
+                            <Nav.Link href="#/home" onClick={handleToggle}>LOGOUT</Nav.Link>
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+        );
+    }
+    else {
+        return (
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" fixed="top">
+                <Container>
+                    <Navbar.Brand href="#home" style={{ 'color': '#ffc800' }}>
+                        MarkIT
+                        <DoneIcon fontSize="large"></DoneIcon>
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="#home">HOME</Nav.Link>
+                            <Nav.Link href="/#/login" >SIGN IN</Nav.Link>
+                        </Nav>
+
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        );
+    }
+}
+export default NavigationBar;
